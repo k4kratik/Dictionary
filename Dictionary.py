7d769a712f6ce70eb1a707e57d6ebdab25a8753c
Kratik Jain <kratik1998@gmail.com>
2018-08-16 11:27:53 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,34 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif  w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w,data.keys())) > 0 :
+        yn = input("Did You Mean  '%s'  instead ??? Enter y if YES or n for No -  " % get_close_matches(w,data.keys())[0])
+        if yn == "y":
+            print("\n%s" % get_close_matches(w,data.keys())[0])
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn=="n":
+            return "The Word doesn't exist, Please Re-Check-it"
+        else:
+            return "You can enter only 'y' or 'n'(Case-Sensitive). "
+    else:
+        return "The Word doesn't exist, Please Re-Check-it"
+
+word = input("Enter A word:  ")
+output = translate(word)
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
+
+input("\n\n\n\nPress ENTER to close window")
